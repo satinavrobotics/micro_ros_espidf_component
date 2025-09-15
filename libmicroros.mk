9d24640d110460d474279880a75cda2f8d0780a0
Czimber Márk <czimbermark@gmail.com>
2025-09-15 06:34:13 +0200

    mtu 1200 for faster publish

--- libmicroros.mk
+++ libmicroros.mk
@@ -67,57 +67,66 @@ $(EXTENSIONS_DIR)/micro_ros_dev/install:
 	CC=clang CXX=clang++ colcon build $(COLCON_HOST_ARGS) --packages-skip ament_index_cpp;
 
 $(EXTENSIONS_DIR)/micro_ros_src/src:
-	rm -rf micro_ros_src; \
-	mkdir micro_ros_src; cd micro_ros_src; \
+	rm -rf $(UROS_DIR); \
+	mkdir -p $(UROS_DIR)/src; \
 	if [ "$(MIDDLEWARE)" = "embeddedrtps" ]; then \
-		git clone -b humble https://github.com/micro-ROS/embeddedRTPS src/embeddedRTPS; \
-		git clone -b humble https://github.com/micro-ROS/rmw_embeddedrtps src/rmw_embeddedrtps; \
+		git clone -b humble https://github.com/micro-ROS/embeddedRTPS $(UROS_DIR)/src/embeddedRTPS; \
+		git clone -b humble https://github.com/micro-ROS/rmw_embeddedrtps $(UROS_DIR)/src/rmw_embeddedrtps; \
 	else \
-		git clone -b ros2 https://github.com/eProsima/Micro-XRCE-DDS-Client src/Micro-XRCE-DDS-Client; \
-		git clone -b humble https://github.com/micro-ROS/rmw_microxrcedds src/rmw_microxrcedds; \
+		git clone -b ros2 https://github.com/eProsima/Micro-XRCE-DDS-Client $(UROS_DIR)/src/Micro-XRCE-DDS-Client; \
+		git clone -b humble https://github.com/micro-ROS/rmw_microxrcedds $(UROS_DIR)/src/rmw_microxrcedds; \
 	fi; \
-	git clone -b ros2 https://github.com/eProsima/micro-CDR src/micro-CDR; \
-	git clone -b humble https://github.com/micro-ROS/rcl src/rcl; \
-	git clone -b humble https://github.com/ros2/rclc src/rclc; \
-	git clone -b humble https://github.com/micro-ROS/rcutils src/rcutils; \
-	git clone -b humble https://github.com/micro-ROS/micro_ros_msgs src/micro_ros_msgs; \
-	git clone -b humble https://github.com/micro-ROS/rosidl_typesupport src/rosidl_typesupport; \
-	git clone -b humble https://github.com/micro-ROS/rosidl_typesupport_microxrcedds src/rosidl_typesupport_microxrcedds; \
-	git clone -b humble https://github.com/ros2/rosidl src/rosidl; \
-	git clone -b humble https://github.com/ros2/rmw src/rmw; \
-	git clone -b humble https://github.com/ros2/rcl_interfaces src/rcl_interfaces; \
-	git clone -b humble https://github.com/ros2/rosidl_defaults src/rosidl_defaults; \
-	git clone -b humble https://github.com/ros2/unique_identifier_msgs src/unique_identifier_msgs; \
-	git clone -b humble https://github.com/ros2/common_interfaces src/common_interfaces; \
-	git clone -b humble https://github.com/ros2/example_interfaces src/example_interfaces; \
-	git clone -b humble https://github.com/ros2/test_interface_files src/test_interface_files; \
-	git clone -b humble https://github.com/ros2/rmw_implementation src/rmw_implementation; \
-	git clone -b humble https://github.com/ros2/rcl_logging src/rcl_logging; \
-	git clone -b humble https://gitlab.com/ros-tracing/ros2_tracing src/ros2_tracing; \
-	touch src/ros2_tracing/tracetools_read/COLCON_IGNORE; \
-	touch src/ros2_tracing/tracetools_trace/COLCON_IGNORE; \
-	touch src/ros2_tracing/tracetools_launch/COLCON_IGNORE; \
-	touch src/ros2_tracing/tracetools_test/COLCON_IGNORE; \
-	touch src/ros2_tracing/ros2trace/COLCON_IGNORE; \
-	git clone -b humble https://github.com/micro-ROS/micro_ros_utilities src/micro_ros_utilities; \
-    touch src/rosidl/rosidl_typesupport_introspection_cpp/COLCON_IGNORE; \
-    touch src/rcl_logging/rcl_logging_log4cxx/COLCON_IGNORE; \
-    touch src/rcl_logging/rcl_logging_spdlog/COLCON_IGNORE; \
-    touch src/rclc/rclc_examples/COLCON_IGNORE; \
-	touch src/rcl/rcl_yaml_param_parser/COLCON_IGNORE; \
-	cp -rf $(EXTRA_ROS_PACKAGES) src/extra_packages || :; \
-	test -f src/extra_packages/extra_packages.repos && cd src/extra_packages && vcs import --input extra_packages.repos || :;
+	# Force MTU=1200 in Micro-XRCE-DDS client header before building libs
+	if [ -f $(UROS_DIR)/src/Micro-XRCE-DDS-Client/include/uxr/client/config.h ]; then \
+		sed -E -i.bak 's/#define UXR_CONFIG_UDP_TRANSPORT_MTU[[:space:]]+512/#define UXR_CONFIG_UDP_TRANSPORT_MTU 1200/' $(UROS_DIR)/src/Micro-XRCE-DDS-Client/include/uxr/client/config.h || true; \
+		rm -f $(UROS_DIR)/src/Micro-XRCE-DDS-Client/include/uxr/client/config.h.bak || true; \
+	fi; \
+	git clone -b ros2 https://github.com/eProsima/micro-CDR $(UROS_DIR)/src/micro-CDR; \
+	git clone -b humble https://github.com/micro-ROS/rcl $(UROS_DIR)/src/rcl; \
+	git clone -b humble https://github.com/ros2/rclc $(UROS_DIR)/src/rclc; \
+	git clone -b humble https://github.com/micro-ROS/rcutils $(UROS_DIR)/src/rcutils; \
+	git clone -b humble https://github.com/micro-ROS/micro_ros_msgs $(UROS_DIR)/src/micro_ros_msgs; \
+	git clone -b humble https://github.com/micro-ROS/rosidl_typesupport $(UROS_DIR)/src/rosidl_typesupport; \
+	git clone -b humble https://github.com/micro-ROS/rosidl_typesupport_microxrcedds $(UROS_DIR)/src/rosidl_typesupport_microxrcedds; \
+	git clone -b humble https://github.com/ros2/rosidl $(UROS_DIR)/src/rosidl; \
+	git clone -b humble https://github.com/ros2/rmw $(UROS_DIR)/src/rmw; \
+	git clone -b humble https://github.com/ros2/rcl_interfaces $(UROS_DIR)/src/rcl_interfaces; \
+	git clone -b humble https://github.com/ros2/rosidl_defaults $(UROS_DIR)/src/rosidl_defaults; \
+	git clone -b humble https://github.com/ros2/unique_identifier_msgs $(UROS_DIR)/src/unique_identifier_msgs; \
+	git clone -b humble https://github.com/ros2/common_interfaces $(UROS_DIR)/src/common_interfaces; \
+	git clone -b humble https://github.com/ros2/example_interfaces $(UROS_DIR)/src/example_interfaces; \
+	git clone -b humble https://github.com/ros2/test_interface_files $(UROS_DIR)/src/test_interface_files; \
+	git clone -b humble https://github.com/ros2/rmw_implementation $(UROS_DIR)/src/rmw_implementation; \
+	git clone -b humble https://github.com/ros2/rcl_logging $(UROS_DIR)/src/rcl_logging; \
+	git clone -b humble https://gitlab.com/ros-tracing/ros2_tracing $(UROS_DIR)/src/ros2_tracing; \
+	touch $(UROS_DIR)/src/ros2_tracing/tracetools_read/COLCON_IGNORE || true; \
+	touch $(UROS_DIR)/src/ros2_tracing/tracetools_trace/COLCON_IGNORE || true; \
+	touch $(UROS_DIR)/src/ros2_tracing/tracetools_launch/COLCON_IGNORE || true; \
+	touch $(UROS_DIR)/src/ros2_tracing/tracetools_test/COLCON_IGNORE || true; \
+	touch $(UROS_DIR)/src/ros2_tracing/ros2trace/COLCON_IGNORE || true; \
+	git clone -b humble https://github.com/micro-ROS/micro_ros_utilities $(UROS_DIR)/src/micro_ros_utilities; \
+	touch $(UROS_DIR)/src/rosidl/rosidl_typesupport_introspection_cpp/COLCON_IGNORE || true; \
+	touch $(UROS_DIR)/src/rcl_logging/rcl_logging_log4cxx/COLCON_IGNORE || true; \
+	touch $(UROS_DIR)/src/rcl_logging/rcl_logging_spdlog/COLCON_IGNORE || true; \
+	touch $(UROS_DIR)/src/rclc/rclc_examples/COLCON_IGNORE || true; \
+	touch $(UROS_DIR)/src/rcl/rcl_yaml_param_parser/COLCON_IGNORE || true; \
+	cp -rf $(EXTRA_ROS_PACKAGES) $(UROS_DIR)/src/extra_packages || :; \
+	test -f $(UROS_DIR)/src/extra_packages/extra_packages.repos && cd $(UROS_DIR)/src/extra_packages && vcs import --input extra_packages.repos || :;
 
 
 $(EXTENSIONS_DIR)/micro_ros_src/install: $(EXTENSIONS_DIR)/esp32_toolchain.cmake $(EXTENSIONS_DIR)/micro_ros_dev/install $(EXTENSIONS_DIR)/micro_ros_src/src
 	cd $(UROS_DIR); \
-	unset AMENT_PREFIX_PATH; \
-	unset ROS_DISTRO; \
-	unset ROS_VERSION; \
-	export CMAKE_BUILD_PARALLEL_LEVEL=1; \
-	export COLCON_DEFAULTS_FILE=/dev/null; \
+	cd $(UROS_DIR); \
+	if [ ! -d src/micro-CDR ]; then \
+		mkdir -p src; if [ -d ../src ]; then cp -R ../src/* src/; fi; \
+	fi; \
+	unset AMENT_PREFIX_PATH; unset ROS_DISTRO; unset ROS_VERSION; \
+	export CMAKE_BUILD_PARALLEL_LEVEL=1; export COLCON_DEFAULTS_FILE=/dev/null; \
 	PATH="$(subst /opt/ros/$(ROS_DISTRO)/bin,,$(PATH))"; \
-	. ../micro_ros_dev/install/local_setup.sh; \
+	if [ ! -f $(EXTENSIONS_DIR)/micro_ros_dev/install/local_setup.sh ]; then \
+		$(MAKE) -j1 -f $(EXTENSIONS_DIR)/libmicroros.mk $(EXTENSIONS_DIR)/micro_ros_dev/install; \
+	fi; \
+	. $(EXTENSIONS_DIR)/micro_ros_dev/install/local_setup.sh; \
 	unset CC; unset CXX; \
 	colcon build \
 		--executor sequential \
@@ -125,7 +134,7 @@ $(EXTENSIONS_DIR)/micro_ros_src/install: $(EXTENSIONS_DIR)/esp32_toolchain.cmake
 		--event-handlers console_direct+ \
 		--merge-install \
 		--packages-ignore-regex=.*_cpp \
-		--packages-skip tracetools_read tracetools_trace tracetools_launch tracetools_test ros2trace \
+		--packages-skip tracetools_read tracetools_trace tracetools_launch tracetools_test ros2trace micro_ros_espidf \
 		--metas $(EXTENSIONS_DIR)/colcon.meta $(APP_COLCON_META) \
 		--cmake-args \
 		"--no-warn-unused-cli" \
@@ -145,9 +154,17 @@ $(EXTENSIONS_DIR)/micro_ros_src/install: $(EXTENSIONS_DIR)/esp32_toolchain.cmake
 		-DUCLIENT_PROFILE_POSIX_TRANSPORT=OFF \
 		-DUCLIENT_PROFILE_UDP_TRANSPORT=ON \
 		-DUXR_CONFIG_UDP_TRANSPORT_MTU=1200 \
+		-DUXR_CONFIG_CUSTOM_TRANSPORT_MTU=1200 \
 		-DRMW_UXRCE_STREAM_HISTORY_INPUT=8 \
 		-DRMW_UXRCE_STREAM_HISTORY_OUTPUT=32 \
 		-DRMW_UXRCE_MAX_PUBLISHERS=6
+
+		# Enforce MTU=1200 in generated config header (covers UDP and CUSTOM)
+		if [ -f $(UROS_DIR)/install/include/uxr/client/config.h ]; then \
+			sed -E -i.bak 's/(#define UXR_CONFIG_UDP_TRANSPORT_MTU)[[:space:]]+[0-9]+/\1 1200/' $(UROS_DIR)/install/include/uxr/client/config.h; \
+			sed -E -i.bak 's/(#define UXR_CONFIG_CUSTOM_TRANSPORT_MTU)[[:space:]]+[0-9]+/\1 1200/' $(UROS_DIR)/install/include/uxr/client/config.h; \
+			rm -f $(UROS_DIR)/install/include/uxr/client/config.h.bak; \
+		fi;
 
 patch_atomic:$(EXTENSIONS_DIR)/micro_ros_src/install
 # Workaround https://github.com/micro-ROS/micro_ros_espidf_component/issues/18
